ff6b3961a18294218cab39ca0dfd44025d5bc41b
realyn <realyn2123@users.noreply.github.com>
2020-11-26 13:06:10 +0800

    init hello world

---
+++ HelloWorldCsharp/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Realyn Racines
+ * Date: 26/11/2020
+ * Time: 12:55 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace HelloWorldCsharp
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
